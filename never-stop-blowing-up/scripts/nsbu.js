aabd6d601a0be32e9ae1b50b50db6093f9a254fe
Blake Keller <blake.keller@gmail.com>
2025-09-03 17:52:35 -0400

    feat: implement NSBUActorSheet and associated system structure with initial stats and UI

---
+++ never-stop-blowing-up/scripts/nsbu.js
@@ -0,0 +1,42 @@
+class NSBUActorSheet extends ActorSheet {
+  static get defaultOptions() {
+    return foundry.utils.mergeObject(super.defaultOptions, {
+      classes: ["never-stop-blowing-up", "sheet", "actor"],
+      template: "systems/never-stop-blowing-up/sheet.html",
+      width: 600,
+      height: 400
+    });
+  }
+
+  async _updateObject(event, formData) {
+    console.log("Updating actor with:", formData);
+    await this.actor.update(formData);
+  }
+}
+
+Hooks.once("init", () => {
+  game.system.model = {
+    Actor: {
+      character: {
+        system: {
+          stats: {
+            grit: { type: Number, default: 1 },
+            fight: { type: Number, default: 1 },
+            flight: { type: Number, default: 1 },
+            brains: { type: Number, default: 1 },
+            charm: { type: Number, default: 1 },
+            brawn: { type: Number, default: 1 }
+          },
+          hp: { type: Number, default: 10 },
+          boomLevel: { type: Number, default: 1 }
+        }
+      }
+    }
+  };
+
+  Actors.unregisterSheet("core", ActorSheet);
+  Actors.registerSheet("never-stop-blowing-up", NSBUActorSheet, {
+    types: ["character"],
+    makeDefault: true
+  });
+});
